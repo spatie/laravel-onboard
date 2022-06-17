58a29cc3536b49f4548c49f9f8b99bfce65b0178
Rias <hey@rias.be>
2022-06-17 11:11:21 +0200

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,35 +2,15 @@
 
 namespace Spatie\Onboard\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Spatie\Onboard\OnboardServiceProvider;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Spatie\\Onboard\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
     protected function getPackageProviders($app)
     {
         return [
             OnboardServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_laravel-onboard_table.php.stub';
-        $migration->up();
-        */
     }
 }
